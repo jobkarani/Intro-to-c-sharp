85899fb8b19a82a91d702417b5a52100f29282dd
Ayub254 <jobkarani084j@gmail.com>
2022-07-14 00:22:07 +0300

    initial commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, Job!");
+
+using System;
+
+namespace GradeBook{
+
+    class Program{
+
+        static void Main(string[]args)
+        {
+            if(args.Length>0){
+
+                // Console.WriteLine("Hello " + args[0] + "!");
+                Console.WriteLine($"Hello, {args[0]} !");
+
+            }
+            else{
+
+                Console.WriteLine("Hello!");
+                
+            }
+            
+        }
+    }
+}
+
